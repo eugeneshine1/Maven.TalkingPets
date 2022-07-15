0dd21f1b9c6cfabf4b71f5327f0a91e168662968
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-14 23:49:14 -0400

    done

---
+++ src/main/java/io/zipcoder/polymorphism/Fish.java
@@ -0,0 +1,11 @@
+package io.zipcoder.polymorphism;
+
+public class Fish extends Pet{
+    public Fish(String name) {
+        super(name);
+    }
+    @Override
+    public String speak() {
+        return "blob";
+    }
+}
--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,65 @@
 package io.zipcoder.polymorphism;
 
+
+
+import java.sql.SQLOutput;
+import java.util.*;
+
 public class MainApplication {
+    public static void main (String [] pets) {
+
+        System.out.println("How many pets do you have?");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+//        HashMap<String,String> petss = new HashMap<>();
+//        for (int x=0; x<num; x++) {
+//            System.out.println("What kind of animal is pet number " + x + "?");
+//            Scanner scan1 = new Scanner(System.in);
+//            String type = scan1.nextLine();
+//            System.out.println("What is pet number " + x + "'s name?");
+//            Scanner scan2 = new Scanner(System.in);
+//            String name = scan2.nextLine();
+//            if (x<num) {
+//                petss.put(type,name);
+//            }
+//        }
+//        System.out.println(petss.entrySet());
+        Pet [] petty = new Pet[num];
+        for (int x = 0; x<num; x++) {
+            System.out.println("Is pet number " + x + " a dog, cat, or fish?");
+            String type = scan.next().toLowerCase();
+            String name = "";
+            switch (type) {
+                case "dog":
+                    System.out.println("What is the dog's name?");
+                    name = scan.next().toLowerCase();
+                    petty[x] = new Dog(name);
+                    break;
+                case "cat":
+                    System.out.println("What is the cat's name?");
+                    name = scan.next();
+                    petty[x] = new Cat(name);
+                    break;
+                case "fish":
+                    System.out.println("What is the fish's name?");
+                    name = scan.nextLine();
+                    petty[x] = new Fish(name);
+                    break;
+                default:
+                    System.out.println("Not my pet...");
+                    break;
+            }
+        }
+            for (Pet pet : petty) {
+                System.out.println(pet.getName()+ " says " +pet.speak());
+            }
+
+
+
+
+
+    }
+
+
 }
+
